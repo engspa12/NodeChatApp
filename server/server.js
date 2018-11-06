0b55bd22138a8f3cc9cb8d3cc836b278648466f6
Dani <arturo_lpc@hotmail.com>
2018-11-05 20:28:16 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -28,6 +28,18 @@ io.on('connection',(socket) => {
   //   createdAt: new Date().getTime()
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   //Event Listener
   socket.on('createMessage',(message) => {
     console.log('createMessage', message);
@@ -38,6 +50,11 @@ io.on('connection',(socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // });
   });
 
   //Event Listener
